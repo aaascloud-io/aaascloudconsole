95b389a2f179f3c6e5649c8ec35096bec782eb13
wang <xiangming.wang@i-focus.co.jp>
2020-12-16 20:28:25 +0900

    プロジェクト登録

---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/constant/AliveConstant.java
@@ -0,0 +1,8 @@
+package com.ifocus.aaascloud.constant;
+
+public class AliveConstant {
+
+	// 1=ALIVE
+	public static final Integer ALIVE = 1;
+
+}
---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/constant/GroupConstant.java
@@ -0,0 +1,8 @@
+package com.ifocus.aaascloud.constant;
+
+public class GroupConstant {
+
+	// 未設定
+	public static final Integer NOT_SET = 0;
+
+}
