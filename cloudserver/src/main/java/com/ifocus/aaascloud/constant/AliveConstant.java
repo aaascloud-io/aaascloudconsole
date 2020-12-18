08819a02f08117a71f0729e24a4ba5a59d4ddae0
wang <xiangming.wang@i-focus.co.jp>
2020-12-18 16:22:06 +0900

    commit

--- cloudserver/src/main/java/com/ifocus/aaascloud/constant/AliveConstant.java
+++ cloudserver/src/main/java/com/ifocus/aaascloud/constant/AliveConstant.java
@@ -5,4 +5,7 @@ public class AliveConstant {
 	// 1=ALIVE
 	public static final Integer ALIVE = 1;
 
+	// 0=NOT ALIVE(Deleted)
+	public static final Integer NOT_ALIVE = 0;
+
 }
---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/constant/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.ifocus.aaascloud.constant;
+
+public class CommonConstant {
+
+	// グループ未設定
+	public static final Integer GROUP_NOT_SET = 0;
+	// プロジェクト未設定
+	public static final Integer PROJECT_NOT_SET = 0;
+
+}
