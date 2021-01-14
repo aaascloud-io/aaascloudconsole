f66daf5a202512c9ad3d9a8ccf10178920481e50
wang <xiangming.wang@i-focus.co.jp>
2021-01-14 18:17:23 +0900

    commit

---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/model/Cloud_errlogModel.java
@@ -0,0 +1,62 @@
+package com.ifocus.aaascloud.model;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Cloud_errlogModel {
+
+	@Getter
+	@Setter
+	private Integer rowid;
+	@Getter
+	@Setter
+	private Integer userid;
+	@Getter
+	@Setter
+	private String device;
+	@Getter
+	@Setter
+	private Integer statusflag;
+	@Getter
+	@Setter
+	private Timestamp datatime;
+	@Getter
+	@Setter
+	private String systemsort;
+	@Getter
+	@Setter
+	private String systemid;
+	@Getter
+	@Setter
+	private String componentid;
+	@Getter
+	@Setter
+	private String messageid;
+	@Getter
+	@Setter
+	private String messagesort;
+	@Getter
+	@Setter
+	private String errcode;
+	@Getter
+	@Setter
+	private String errMessage;
+	@Getter
+	@Setter
+	private Integer alive;
+	@Getter
+	@Setter
+	private Integer i_uid;
+	@Getter
+	@Setter
+	private Timestamp i_time;
+	@Getter
+	@Setter
+	private Integer u_uid;
+	@Getter
+	@Setter
+	private Timestamp u_time;
+
+}
