a5181070b950623249e61f916ccd315b7a69106d
wang <xiangming.wang@i-focus.co.jp>
2021-01-15 21:24:26 +0900

    commit

--- cloudserver/src/main/java/com/ifocus/aaascloud/entity/Cloud_companyRepository.java
+++ cloudserver/src/main/java/com/ifocus/aaascloud/entity/Cloud_companyRepository.java
@@ -1,8 +1,19 @@
 package com.ifocus.aaascloud.entity;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.ifocus.aaascloud.model.Cloud_userModel;
 
 //@Repository
 public interface  Cloud_companyRepository extends CrudRepository<Cloud_companyEntity, Integer> {
 
+	/*
+	 * 会社情報取得
+	 *
+	 *
+	 */
+	@Query(value = "SELECT c.*,d.corporatenumber FROM cloud_company c INNER JOIN cloud_user d ON c.companyid = d.companyid WHERE d.userid = :userid",nativeQuery = true)
+	public Cloud_userModel searchCompanyByUserid(@Param("userid") Integer userid);
 }
--- cloudserver/src/main/java/com/ifocus/aaascloud/entity/Cloud_deviceRepository.java
+++ cloudserver/src/main/java/com/ifocus/aaascloud/entity/Cloud_deviceRepository.java
@@ -2,8 +2,7 @@ package com.ifocus.aaascloud.entity;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -15,8 +14,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT c FROM cloud_device c WHERE c.projectid = 0 AND c.companyid = :companyid")
-	@Autowired
+	@Query(value = "SELECT c.* FROM cloud_device c WHERE c.projectid = 0 AND c.companyid = :companyid", nativeQuery = true)
 	public List<Cloud_deviceEntity> searchSelectableDevicesByCompanyid(@Param("companyid") Integer companyid);
 
 	/*
@@ -24,8 +22,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT c FROM cloud_device c WHERE c.companyid IN :companyids ORDER BY c.imei")
-	@Autowired
+	@Query(value = "SELECT c.* FROM cloud_device c WHERE c.companyid IN :companyids ORDER BY c.imei", nativeQuery = true)
 	public List<Cloud_deviceEntity> searchUnderCompanyDevicesByCompanyidIn(@Param("companyids") List<Integer> companyids);
 
 	/*
@@ -50,7 +47,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query(   "SELECT d "
+	@Query(value = "SELECT d.* "
 			+ "FROM cloud_device d "
 			+ "INNER JOIN cloud_product pd ON d.productId = pd.productId "
 			+ "INNER JOIN cloud_project pj ON d.projectId = pj.projectId "
@@ -64,8 +61,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 			+ "AND pj.projectName LIKE :projectname "
 			+ "AND com.industry LIKE :industry "
 			+ "AND g.groupname LIKE :groupname "
-			+ "ORDER BY d.companyId,d.imei")
-	@Autowired
+			+ "ORDER BY d.companyId,d.imei", nativeQuery = true)
 	public List<Cloud_deviceEntity> findByCompanyidInAndImeiLikeOrIccidLikeOrSnLikeAndProduct_ProductnameLikeAndProject_ProjectnameLikeAndCompany_IndustryLikeAndGroupentity_GroupnameLike(
 			@Param("companyids") List<Integer> companyids,
 			@Param("imei") String imei,
@@ -78,21 +74,39 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 		);
 
 	/*
-	 * 配下各社デバイス検索(デバイス管理画面用:グループ検索あり)
+	 * 配下各社デバイス検索(デバイス管理画面用:グループ検索なし)
 	 *
 	 *
 	 */
-	@Query()
-	@Autowired
-	public List<Cloud_deviceEntity> findByCompanyidIn(@Param("companyids") List<Integer> companyids);
+	@Query(value = "SELECT d.* "
+			+ "FROM cloud_device d "
+			+ "INNER JOIN cloud_product pd ON d.productId = pd.productId "
+			+ "INNER JOIN cloud_project pj ON d.projectId = pj.projectId "
+			+ "INNER JOIN cloud_company com ON d.companyId = com.companyId "
+			+ "WHERE d.companyId IN :companyids "
+			+ "AND (d.IMEI LIKE :imei "
+			+ "    OR d.ICCID LIKE :iccid "
+			+ "    OR d.SN LIKE :sn) "
+			+ "AND pd.productName LIKE :productname "
+			+ "AND pj.projectName LIKE :projectname "
+			+ "AND com.industry LIKE :industry "
+			+ "ORDER BY d.companyId,d.imei", nativeQuery = true)
+	public List<Cloud_deviceEntity> findByCompanyidInAndImeiLikeOrIccidLikeOrSnLikeAndProduct_ProductnameLikeAndProject_ProjectnameLikeAndCompany_IndustryLike(
+			@Param("companyids") List<Integer> companyids,
+			@Param("imei") String imei,
+			@Param("iccid") String iccid,
+			@Param("sn") String sn,
+			@Param("productname") String productName,
+			@Param("projectname") String projectName,
+			@Param("industry") String industry
+		);
 
 	/*
 	 * 自社全デバイス一覧(デバイス管理画面用)
 	 *
 	 *
 	 */
-	@Query("SELECT c FROM cloud_device c WHERE c.companyid = :companyid ORDER BY c.imei")
-	@Autowired
+	@Query(value = "SELECT c.* FROM cloud_device c WHERE c.companyid = :companyid ORDER BY c.imei", nativeQuery = true)
 	public List<Cloud_deviceEntity> searchAllDevicesByCompanyid(@Param("companyid") Integer companyid);
 
 	/*
@@ -100,8 +114,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT c FROM cloud_device c WHERE c.projectid = :projectid")
-	@Autowired
+	@Query(value = "SELECT c.* FROM cloud_device c WHERE c.projectid = :projectid", nativeQuery = true)
 	public List<Cloud_deviceEntity> searchDevicesByProjectid(@Param("projectid") Integer projectid);
 
 	/*
@@ -109,8 +122,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT COUNT(c) FROM cloud_device c WHERE c.projectid = :projectid")
-	@Autowired
+	@Query(value = "SELECT COUNT(c) FROM cloud_device c WHERE c.projectid = :projectid", nativeQuery = true)
 	public Integer getProjectDeviceCountsByProjectid(@Param("projectid") Integer projectid);
 
 	/*
@@ -118,8 +130,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT c FROM cloud_device c WHERE c.projectid = :projectid AND c.groupid = :groupid")
-	@Autowired
+	@Query(value = "SELECT c.* FROM cloud_device c WHERE c.projectid = :projectid AND c.groupid = :groupid", nativeQuery = true)
 	public List<Cloud_deviceEntity> searchDevicesByProjectidAndGroupid(@Param("projectid") Integer projectid, @Param("groupid") Integer groupid);
 
 	/*
@@ -127,8 +138,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT COUNT(c) FROM cloud_device c WHERE c.projectid = :projectid AND c.groupid = :groupid")
-	@Autowired
+	@Query(value = "SELECT COUNT(c) FROM cloud_device c WHERE c.projectid = :projectid AND c.groupid = :groupid", nativeQuery = true)
 	public Integer getGroupDeviceCountsByProjectidAndGroupid(@Param("projectid") Integer projectid, @Param("groupid") Integer groupid);
 
 	/*
@@ -136,8 +146,7 @@ public interface  Cloud_deviceRepository extends CrudRepository<Cloud_deviceEnti
 	 *
 	 *
 	 */
-	@Query("SELECT c FROM cloud_device c WHERE c.userid IN :userids ORDER BY c.productid,c.imei")
-	@Autowired
+	@Query(value = "SELECT c.* FROM cloud_device c WHERE c.userid IN :userids ORDER BY c.productid,c.imei", nativeQuery = true)
 	public List<Cloud_deviceEntity> searchUnderCompanyDevicesByUseridIn(@Param("userids") List<Integer> userids);
 
 }
