ce1c0c12ac5b12d92f91aec7026ff522b3bf239b
wang <xiangming.wang@i-focus.co.jp>
2021-01-26 19:59:48 +0900

    エラー履歴機能

---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/constant/StatusFlagConstant.java
@@ -0,0 +1,12 @@
+package com.ifocus.aaascloud.constant;
+
+public class StatusFlagConstant {
+
+	// 未対応
+	public static final Integer FLAG_NEW = 0;
+	// 対応中(Work In Progress)
+	public static final Integer FLAG_WIP = 1;
+	// 対応済
+	public static final Integer FLAG_DONE = 9;
+
+}
