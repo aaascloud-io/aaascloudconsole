7855f46d3c21f9dce7ea23870558d01d874ae43a
wang <xiangming.wang@i-focus.co.jp>
2021-02-09 19:02:20 +0900

    テーブル追加

---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/entity/Cloud_producttypeRepository.java
@@ -0,0 +1,9 @@
+package com.ifocus.aaascloud.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+//@Repository
+public interface  Cloud_producttypeRepository extends CrudRepository<Cloud_producttypeEntity, Integer> {
+
+
+}
