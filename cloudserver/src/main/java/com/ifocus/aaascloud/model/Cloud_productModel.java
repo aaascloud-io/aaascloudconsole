c2f9eb27116e5450c554cc7dc47b22c2888a6ea9
wang <xiangming.wang@i-focus.co.jp>
2021-02-09 19:04:01 +0900

    テーブル追加

--- cloudserver/src/main/java/com/ifocus/aaascloud/model/Cloud_productModel.java
+++ cloudserver/src/main/java/com/ifocus/aaascloud/model/Cloud_productModel.java
@@ -3,37 +3,21 @@ package com.ifocus.aaascloud.model;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class Cloud_productModel {
 
-	@Getter
-	@Setter
 	private LoginInfo loginInfo;
-	@Getter
-	@Setter
 	private TargetUserInfo targetUserInfo;
-	@Getter
-	@Setter
+
 	private Integer productid;
-	@Getter
-	@Setter
+	private Integer producttypeid;
 	private String productcode;
-	@Getter
-	@Setter
 	private String productname;
-	@Getter
-	@Setter
 	private String model;
-	@Getter
-	@Setter
 	private String version;
-	@Getter
-	@Setter
 	private Integer simflag;
-	@Getter
-	@Setter
 	private String summary;
-	@Getter
-	@Setter
 	private Integer alive;
 
 }
