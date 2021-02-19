311e540bb07662a16d9c3221b1b153a00938f1cb
wang <xiangming.wang@i-focus.co.jp>
2021-02-19 11:54:11 +0900

    デバイス項目追加

--- cloudserver/src/main/java/com/ifocus/aaascloud/model/Cloud_deviceDetailModel.java
+++ cloudserver/src/main/java/com/ifocus/aaascloud/model/Cloud_deviceDetailModel.java
@@ -1,77 +1,43 @@
 package com.ifocus.aaascloud.model;
 
+import java.sql.Timestamp;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class Cloud_deviceDetailModel {
 
-	@Getter
-	@Setter
 	private LoginInfo loginInfo;
-	@Getter
-	@Setter
 	private TargetUserInfo targetUserInfo;
-	@Getter
-	@Setter
+
 	private Integer productid;
-	@Getter
-	@Setter
 	private String productcode;
-	@Getter
-	@Setter
 	private String productname;
-	@Getter
-	@Setter
 	private Integer deviceid;
-	@Getter
-	@Setter
 	private Integer projectid;
-	@Getter
-	@Setter
 	private String projectname;
-	@Getter
-	@Setter
 	private Integer groupid;
-	@Getter
-	@Setter
 	private String groupname;
-	@Getter
-	@Setter
 	private String devicename;
-	@Getter
-	@Setter
 	private String imei;
-	@Getter
-	@Setter
 	private String iccid;
-	@Getter
-	@Setter
 	private String sn;
-	@Getter
-	@Setter
 	private String sim_iccid;
-	@Getter
-	@Setter
-	private String sim_imei;
-	@Getter
-	@Setter
+	private String sim_imsi;
 	private String sim_tel;
-	@Getter
-	@Setter
+	private Integer encryptedcommunications;
+	private String encryptedkey;
+	private String connectserverurl;
+	private String connectserverport;
+	private Integer bindingflag;
+	private String fmlastestversion;
+	private Timestamp versioncomfirmtime;
 	private Integer companyid;
-	@Getter
-	@Setter
 	private String industry;
-	@Getter
-	@Setter
 	private Integer userid;
-	@Getter
-	@Setter
 	private Integer lastprojectId;
-	@Getter
-	@Setter
 	private Integer lastgroupid;
-	@Getter
-	@Setter
 	private Integer alive;
 }
