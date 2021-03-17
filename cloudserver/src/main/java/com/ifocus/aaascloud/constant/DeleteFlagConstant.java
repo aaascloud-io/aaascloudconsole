b9a5f51cafbea6b563d8d5a1477fe63cf0e9f1d5
wang <xiangming.wang@i-focus.co.jp>
2021-03-17 18:24:54 +0900

    論理削除対応(プロダクト管理)

---
+++ cloudserver/src/main/java/com/ifocus/aaascloud/constant/DeleteFlagConstant.java
@@ -0,0 +1,11 @@
+package com.ifocus.aaascloud.constant;
+
+public class DeleteFlagConstant {
+
+	// 1=削除済行
+	public static final Integer DELETED = 1;
+
+	// 0=通常行(NOT Deleted)
+	public static final Integer NOT_DELETED = 0;
+
+}
