784fcaa2eb8b992656dc507f4e670bb8a4bca14a
wang <xiangming.wang@i-focus.co.jp>
2021-03-19 13:21:06 +0900

    プロダクトタイプTBL⇒定数に変更

--- cloudserver/src/main/java/com/ifocus/aaascloud/entity/Cloud_producttypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifocus.aaascloud.entity;
-
-import org.springframework.data.repository.CrudRepository;
-
-//@Repository
-public interface  Cloud_producttypeRepository extends CrudRepository<Cloud_producttypeEntity, Integer> {
-
-
-}
