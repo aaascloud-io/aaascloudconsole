1e5d0eff2c079ab981e255dab136863397ba230f
yanchuang <chuang.yan@i-focus.co.jp>
2021-06-04 16:44:44 +0900

    ソースロジックを修正しなくて、無視ください

--- web-page-new/src/app/ui/if-modal/if-modal.module.ts
+++ web-page-new/src/app/ui/if-modal/if-modal.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IfModalComponent} from "./if-modal.component";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
     declarations: [IfModalComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     exports: [IfModalComponent],
 })
 export class IfModalModule { }
