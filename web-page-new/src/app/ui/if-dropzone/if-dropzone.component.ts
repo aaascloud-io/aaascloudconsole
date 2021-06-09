3a48d78ddc9fcb8886ee0093cc63544327fae960
yanchuang <chuang.yan@i-focus.co.jp>
2021-06-09 15:54:17 +0900

    ロジックを修正しなくて、コメントを追加する

--- web-page-new/src/app/ui/if-dropzone/if-dropzone.component.ts
+++ web-page-new/src/app/ui/if-dropzone/if-dropzone.component.ts
@@ -1,4 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input, OnInit,
+    Output
+} from '@angular/core';
 
 
 @Component({
@@ -8,29 +13,56 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class IfDropzoneComponent implements OnInit {
 
+    /**
+     * 選択したファイル
+     */
     files: File[] = [];
-    // files: any;
 
+    /**
+     * 複数選択可否
+     */
     @Input() multiple: boolean;
+
+    /**
+     * メッセージ
+     */
     @Input() label: string;
 
     /**
      * Changeイベント
      */
     @Output() onChangeEvent = new EventEmitter<File[]>();
+
+    /**
+     * 削除イベント
+     */
     @Output() onRemoveEvent = new EventEmitter<File[]>();
 
 
-    changeMultipleFile(event) {
-        if (!this.multiple){
+    /**
+     * Changeイベント処理
+     * @param event イベント
+     */
+    changeMultipleFile(event): void {
+        // 複数選択不可の場合
+        if (!this.multiple) {
             this.files = [];
         }
+        // 選択したファイルを追加する
         this.files.push(...event.addedFiles);
+
+        // コールバック関数を呼び出す
         this.onChangeEvent.emit(this.files);
     }
 
-    removeMultipleFile(event) {
+    /**
+     * 削除イベント処理
+     * @param event イベント
+     */
+    removeMultipleFile(event): void {
+        // 削除
         this.files.splice(this.files.indexOf(event), 1);
+        // コールバック関数を呼び出す
         this.onRemoveEvent.emit(this.files);
     }
 
